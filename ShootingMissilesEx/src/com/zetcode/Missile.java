90b1f8b4bd0635038db351eca5aa4dd14ffb012e
David Church <dchurch2000@earthlink.net>
2018-09-03 20:59:18 -0500

    Initial commit

---
+++ ShootingMissilesEx/src/com/zetcode/Missile.java
@@ -0,0 +1,44 @@
+package com.zetcode;
+
+/**
+ * Here we have a new sprite called Missile.
+ * 
+ * public void move() {
+ *     
+ *     x += MISSILE_SPEED;
+ *     
+ *     if (x > BOARD_WIDTH) {
+ *         vis = false;
+ *     }
+ * }
+ * 
+ * The missile moves at constant speed. When it hits the right border of the Board, it becomes invisible. It is then removed from the list of missiles.
+ * SpaceShip.java
+ */
+public class Missile extends Sprite {
+
+    private final int BOARD_WIDTH = 390;
+    private final int MISSILE_SPEED = 2;
+
+    public Missile(int x, int y) {
+        super(x, y);
+        
+        initMissile();
+    }
+    
+    private void initMissile() {
+        
+        loadImage("src/resources/missile.png");  
+        getImageDimensions();
+    }
+
+    public void move() {
+        
+        x += MISSILE_SPEED;
+        
+        if (x > BOARD_WIDTH) {
+            visible = false;
+        }
+    }
+}
+
